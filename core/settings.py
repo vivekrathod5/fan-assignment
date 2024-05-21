5392cbb027772631d0c91bc0355a17e5a1fcdc99
vivekrathod5 <vivekrathod536@gmail.com>
2024-05-22 01:58:56 +0530

    up settings.py for static files

--- core/settings.py
+++ core/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'static/'
-STATIC_ROOT = '/static'
-MEDIA_ROOT = '/media'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Default primary key field type
